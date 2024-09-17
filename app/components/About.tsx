663a5bb7e7fe519076b488fb9f03779b9ef2ea78
sadafshahab12~ <sadafshahab123@yahoo.com>
2024-09-17 17:02:01 +0500

    Create next js app with Header and About components

---
+++ app/components/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+   <>
+   <h1 className='text-2xl font-bold'>This is About</h1>
+   </>
+  )
+}
+
+export default About
---
+++ app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <h1 className='text-2xl font-bold pb-5'> This is Header</h1>
+    </> 
+  );
+}
+
+export default Header;
